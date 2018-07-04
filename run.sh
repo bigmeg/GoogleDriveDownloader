03142a3730832e169234057445dc6fbada9fabd2
root <root@SFO2Ubuntu18.04>
2018-07-04 14:36:24 -0700

    run script permission fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-PORT=$(awk -F "=" '/SERVER_PORT/ {print $2}' settings.ini)
-docker run -d -p $PORT:$PORT -v ~/Downloads/:/root/Downloads gdd
+PORT=$(awk -F "=" '/SERVER_PORT/ {print $2}' settings.ini | xargs)
+docker run  -d -p $PORT:$PORT -v ~/Downloads/:/root/Downloads --restart unless-stopped gdd
